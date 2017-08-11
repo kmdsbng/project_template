ecb30d563d684eb721c38da16f450d453bc8f819
Yoshihiro Kameda <kameda.sbng@gmail.com>
2017-08-11 21:54:06 +0900

    fix to use instance variable

--- almin_typescript_template/src/domain/TodoList/TodoId.ts
+++ almin_typescript_template/src/domain/TodoList/TodoId.ts
@@ -4,23 +4,23 @@ export default class TodoId {
     readonly value: string;
 
     constructor(value: string) {
-        this.checkValid(value);
         this.value = value;
         Object.freeze(this);
+        this.checkValid();
     }
 
     equals(other: TodoId) {
         return this.value == other.value;
     }
 
-    checkValid(value: string) {
-        if (!value) {
+    checkValid() {
+        if (this.value == null) {
             throw new TypeError("Invalid id : null");
         }
-        if (value.length == 0) {
+        if (this.value.length == 0) {
             throw new TypeError("Invalid id : blank string");
         }
-        if (value.length > 50) {
+        if (this.value.length > 50) {
             throw new TypeError("Invalid id : too long");
         }
     }
