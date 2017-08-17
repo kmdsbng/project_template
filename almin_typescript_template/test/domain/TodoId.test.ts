5f1c1bdf4d8acf6dc08c29e4a424de9feb81195f
Yoshihiro Kameda <kameda.sbng@gmail.com>
2017-08-17 23:14:46 +0900

    add test

--- almin_typescript_template/test/domain/TodoId.test.ts
+++ almin_typescript_template/test/domain/TodoId.test.ts
@@ -3,6 +3,10 @@ import test from 'ava';
 import uuid = require("uuid");
 import TodoId from 'domain/TodoList/TodoId';
 
+test("TodoId-test : invalid id : null", (t) => {
+  t.throws(() => {new TodoId(null)});
+});
+
 test("TodoId-test : invalid id : blank", (t) => {
   t.throws(() => {new TodoId("")});
 });
@@ -16,6 +20,13 @@ test("TodoId-test : construct with uuid", function (t) {
     const id = uuid.v1();
     const todoId = new TodoId(id);
     t.is(todoId.value, id);
+});
+
+test("TodoId-test : equals", function (t) {
+    const id = uuid.v1();
+    const todoId = new TodoId(id);
+    const todoId2 = new TodoId(id);
+    t.is(todoId.equals(todoId2), true);
 });
 
 
